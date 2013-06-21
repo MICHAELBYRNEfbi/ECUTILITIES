1ff1dcd9a5658f07b46c8cb38ba8b32a2ee37faa
Michael Byrne <mdb1800@gmail.com>
2013-06-22 08:46:45 +1000

    Starting Commit
    This pushes the files

---
+++ src/me/LOC/utils/onCommand.java
@@ -0,0 +1,39 @@
+package me.LOC.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+ 
+public class onCommand extends JavaPlugin {
+    @Override
+        public void onEnable(){
+        getLogger().info("Starting Elite Utils Watchdog");
+        getLogger().info("feeding him...");
+        getLogger().info("reasurring him...");
+        getLogger().info("Testing him");
+        getLogger().info("Ouch...");
+        getLogger().info("Ok");
+        getLogger().info("sending...");
+        getLogger().info("Plugin loading complete.");
+        }
+ 
+        @Override
+        public void onDisable() {
+            getLogger().info("Elite Utils is being terminated... it hurts...");
+        }
+ 
+        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+    	    Player player = (Player) sender;
+        	if(cmd.getName().equalsIgnoreCase("shop")){  	    
+            player.sendMessage(ChatColor.WHITE + "[" + ChatColor.GOLD + ChatColor.BOLD + "EC" + ChatColor.WHITE + "] " + "Now warping to the server shop!");
+            player.teleport(new Location(Bukkit.getWorld("world"), 80, 142, 11)); }
+            return true;
+        }
+}
+
